becca76c2ff98a713b970446a6a20da574525e51
IDKTHIS <polireek@gmail.com>
2023-10-11 18:59:02 +0300

    1.0.1 updated info

--- JCorePanel/PanelData.cs
+++ JCorePanel/PanelData.cs
@@ -8,9 +8,9 @@ namespace JCorePanel
 {
     public static class PanelData
     {
-        public static string Version = "1.0.0";
-        public static string Date = "15.08.2023";
-        public static string ChangeLog = "- Panel was Realsed";
+        public static string Version = "1.0.1";
+        public static string Date = "11.10.2023";
+        public static string ChangeLog = "- Removed plugin status system\n- Removed support discord\n- Now Work AS IS and repository will no longer be private dont worry.";
 
     }
 }
